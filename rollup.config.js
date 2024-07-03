377d2551b12e8be6a17c8f703d506b8830055c60
Elisabeth Rousset <lisbeth.rousset@gmail.com>
2024-07-03 10:12:43 +0200

    unified buildsystem

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,15 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import del from 'rollup-plugin-delete'
 import { dts } from "rollup-plugin-dts";
+import { readFileSync } from 'fs';
 
 const PACKAGE_NAME = "three-js-disposer"
+const PACKAGE_JSON = JSON.parse(readFileSync('package.json', 'utf8'))
+
+const external = [
+    ...Object.keys(PACKAGE_JSON.dependencies ?? {}),
+    ...Object.keys(PACKAGE_JSON.peerDependencies ?? {})
+]
 
 export default [
     // bundle code
@@ -18,9 +25,7 @@ export default [
             commonjs(),
             nodeResolve(),
         ],
-        external: [
-            /node_modules/,
-        ],
+        external: external,
         output: [
             {
                 dir: `./dist`,
